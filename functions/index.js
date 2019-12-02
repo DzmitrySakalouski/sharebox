b9fcd4e9576be4ce16696a867e4b4a44af3463cb
Dzmitry  Sakalouski <dzmitrysakalouski@MacBook-Pro-Dzmitry.local>
2019-12-02 23:55:07 +0300

    add link

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,4 @@ admin.initializeApp();
 
 // firebase deploy --only functions https://firebase.google.com/docs/functions/get-started
 // https://stackoverflow.com/questions/43486278/how-do-i-structure-cloud-functions-for-firebase-to-deploy-multiple-functions-fro
+// https://stackoverflow.com/questions/47726099/how-to-write-data-to-firestore-through-firebase-functions
