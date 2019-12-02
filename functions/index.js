b9ad8c4d971193e7b5f32f1c6a2f7a55212ff652
Dzmitry <Sakalouski>
2019-12-02 18:36:04 +0300

    todo

--- functions/index.js
+++ functions/index.js
@@ -2,3 +2,6 @@ const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
 admin.initializeApp();
+
+
+// firebase deploy --only functions https://firebase.google.com/docs/functions/get-started
